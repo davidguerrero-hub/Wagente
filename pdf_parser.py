4aa1a3a55580dc8ee4c31d14e6e3064543e5af99
davidguerrero-hub <dguerrerocata@gmail.com>
2026-08-15 08:35:45 -0400

    parser ambas columnas

--- pdf_parser.py
+++ pdf_parser.py
@@ -1,3 +1,11 @@
 from pypdf import PdfReader
 
-print("hoal")
+reader = PdfReader("W3_ROC_Data.pdf")
+
+print("Número de páginas:", len(reader.pages))
+
+pagina = reader.pages[2]
+
+texto = pagina.extract_text(extraction_mode="layout")
+
+print(texto)
